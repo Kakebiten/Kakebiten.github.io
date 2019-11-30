f690e80629efd7e82176903613f7157cacd1d912
Tobias André Arildset <soulkake@soulnetwork.eu>
2019-11-30 14:09:57 +0100

    Testing download system

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 alert("Welcome to my site!");
+$("#button").click(() => {
+    /*$("#button").attr({target: '_blank',
+                  href: 'res/testimage.png'});*/
+    
+});
+
+download("res/testimage.png")
